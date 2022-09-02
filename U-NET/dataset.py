9c7eb26ba3387b36ed931c567c9d852cdbb39520
sanha9999 <sanha9999@naver.com>
2022-09-02 15:50:06 +0900

    cityscape dataset

--- U-NET/dataset.py
+++ U-NET/dataset.py
@@ -7,9 +7,10 @@ from torch.utils.data import Dataset
 from PIL import Image
 
 class UnetDataset(Dataset):
-    def __init__(self, img_dir):
+    def __init__(self, img_dir, transform = None):
         super(UnetDataset, self).__init__()
         self.img_list = os.listdir(img_dir)
+        self.transform  = transform
 
     def __len__(self):
         return len(self.img_list)
@@ -17,9 +18,17 @@ class UnetDataset(Dataset):
     def __getitem__(self, idx):
         img_path = self.img_list[idx]
         img_path = os.path.join(self.img_list, img_path)
-        img = np.array(Image.open(img_path))
-        img, label = self 
+        img = np.array(Image.open(img_path), )
+        img, label = self.split_image(img)
+
+        if self.transform:
+            img = self.transform(img)
+            label = self.transform(label)
+
+        return img, label
 
     def split_image(self, img):
         img = np.array(img)
-        img, label = img[ : , :256 , : ], img[ : , 256: , :]   
+        img = img / 255.0
+        img, label = img[ : , :256 , : ], img[ : , 256: , :]
+        return img, label
