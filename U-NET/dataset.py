81a4ad95fe6c19a671c2a9ecc9567bc5b8ddd0ef
sanha9999 <sanha9999@naver.com>
2022-09-06 17:54:54 +0900

    dataset, label kmeans

--- U-NET/dataset.py
+++ U-NET/dataset.py
@@ -6,13 +6,22 @@ from torch.utils.data import Dataset
 import pytorch_lightning as pl
 from PIL import Image
 import albumentations
+from sklearn.cluster import KMeans
 
 class CityDataset(Dataset):
-    def __init__(self, img_dir, transform = None):
+    def __init__(self, img_dir, n_classes, transform = None):
         super(CityDataset, self).__init__()
         self.img_dir = img_dir
         self.img_list = os.listdir(img_dir)
-        self.transform  = transform 
+        self.transform  = transform
+        self.n_classes = n_classes
+        # output label define
+
+        num_items = 1000
+        color_array = np.random.choice(range(256), 3 * num_items).reshape(-1, 3)
+
+        self.label_model = KMeans(n_clusters=n_classes)
+        self.label_model.fit(color_array)
 
     def __len__(self):
         return len(self.img_list)
@@ -27,11 +36,15 @@ class CityDataset(Dataset):
             augmented = self.transform(image = img, mask = label)
             img = augmented['image']
             label = augmented['mask']
+        
+        label_class = torch.Tensor(self.label_define(label)).long()
             
-        return img, label
+        return img, label_class
 
     def split_image(self, img):
         img = np.array(img)
-        #img = img / 255.0
         img, label = img[ : , :256 , : ], img[ : , 256: , :]
         return img, label
+
+    def label_define(self, label):
+        return self.label_model.predict(label.reshape(-1, 3)).reshape(256, 256)
